e0f8017e7cf1683025144e7f1b7d99e76f7b3b04
paul <noudurianandpaul@gmail.com>
2025-01-21 05:25:31 +0530

    Added a file

---
+++ python.py
@@ -0,0 +1,15 @@
+a = int(input("Enter a first number :"))
+b = int(input("Enter second number :"))
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+g = a%b
+print("Addition of a and b is :",c)
+print("Subtraction of a and b is :",d)
+print("Multiplication of a and b is :",e)
+print("Division of a and b gives the quotient of :",f)
+print("Division of a and b gives the remainder of :",g)
+
+
+
